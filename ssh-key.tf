e47188dd29587871ad7d1f739a2751860b2ba81f
BinhVo <vothanhbinh.lt@gmail.com>
2021-03-03 00:39:11 +0700

    add ssh-key and master-key

--- ssh-key.tf
+++ ssh-key.tf
@@ -1,4 +1,12 @@
 resource "tls_private_key" "main_key_pair" {
   algorithm = "RSA"
-  rsa_bits  = 4096
+  rsa_bits  = 2048
+}
+
+resource "random_string" "master_key" {
+  length  = "32"
+  special = false
+  lower   = true
+  number  = true
+  upper   = true
 }
